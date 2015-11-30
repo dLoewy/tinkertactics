3343a627d5b64cd0b0cee1f4074d5a8866fc2d77
dLoewy <david.loewy@gmail.com>
2015-11-30 10:17:14 -0500

    Added piece values

--- js/tinkertactics.js
+++ js/tinkertactics.js
@@ -1,8 +1,9 @@
 "use strict";
 
 class Piece {
-    constructor(color){
+    constructor(color, value){
         this.color = color;
+        this.value = value;
         this.selected = false;
     }
 }
@@ -44,13 +45,14 @@ class TTView {
         let matrix = this.board.matrix;
         let boardDiv = document.createElement('div');
         boardDiv.setAttribute('id', 'board');
+        boardDiv.classList.add('unselectable');
         for (let i = 0; i < matrix.length; i++){
             let rowDiv = document.createElement('div');
             rowDiv.classList.add('row');
             for (let j = 0; j < matrix[i].length; j++){
                 let thisCell = matrix[i][j];
                 let cellDiv = document.createElement('div');
-                cellDiv.setAttribute('id', 'cell' + i + '-' + j);
+                cellDiv.setAttribute('id', 'cell-' + i + '-' + j);
                 cellDiv.setAttribute('row', i);
                 cellDiv.setAttribute('col', j);
                 cellDiv.classList.add('cell');
@@ -72,7 +74,7 @@ class TTView {
         for (let i = 0; i < matrix.length; i++){
             for (let j = 0; j < matrix[i].length; j++){
                 let thisCell = matrix[i][j];
-                let cellDiv = document.getElementById('cell' + i + '-' + j);
+                let cellDiv = document.getElementById('cell-' + i + '-' + j);
                 while(cellDiv.firstChild){
                     cellDiv.removeChild(cellDiv.firstChild);
                 }
@@ -83,6 +85,7 @@ class TTView {
                     if(thisCell.piece.selected){
                         pieceDiv.classList.add('selected');
                     }
+                    pieceDiv.innerHTML = thisCell.piece.value;
                     cellDiv.appendChild(pieceDiv);
                 }
             }
@@ -103,29 +106,31 @@ class TTGame {
         if (! this.selectedCell && clickedCell.piece){
             clickedCell.piece.selected = true;
             this.selectedCell = clickedCell;
+            this.view.update();
         }
         // If there IS a selected cell, and an empty cell has been clicked
-        else if (! clickedCell.piece){
+        else if (this.selectedCell && ! clickedCell.piece){
             // Move and unselect the piece
             clickedCell.piece = this.selectedCell.piece;
             clickedCell.piece.selected = false;
             this.selectedCell.piece = null;
             this.selectedCell = null;
+            this.view.update();
         }
         else if (clickedCell === this.selectedCell){
             this.selectedCell = null;
             clickedCell.piece.selected = false;
+            this.view.update();
         }
-        this.view.update();
     }
 
     newGame(){
         let redPiece = new Piece('red');
         let bluePiece = new Piece('blue');
-        this.myBoard.getCell(0,0).piece = new Piece('red');
-        this.myBoard.getCell(0,1).piece = new Piece('blue');
-        this.myBoard.getCell(0,2).piece = new Piece('blue');
-        this.myBoard.getCell(0,3).piece = new Piece('red');
+        this.myBoard.getCell(0,0).piece = new Piece('red', 3);
+        this.myBoard.getCell(0,1).piece = new Piece('blue', 4);
+        this.myBoard.getCell(0,2).piece = new Piece('blue', 2);
+        this.myBoard.getCell(0,3).piece = new Piece('red', 5);
         this.view.initialize();
         this.view.update();
     }
